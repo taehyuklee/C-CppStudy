42c4ebc6370e01605799b84b42b3c59b4f1566ea
TayLee <89365465+taehyuklee@users.noreply.github.com>
2022-08-26 23:41:41 +0900

    Update Printf.cpp

--- InputOutput/Printf.cpp
+++ InputOutput/Printf.cpp
@@ -6,14 +6,23 @@ typedef long long ll;
 
 double a = 1.23456789;
 int b = 2;
+char s = 'a';
+string str = "하이 나는 감팡이야";
+
 
 int main(){
 	printf("%.6lf\n", a);
 	printf("%02d\n", b);
+	printf("문자 하나 : %c\n", s);
+	printf("문자열 출력 : %s\n", str.c_str());
 	//C언어 처럼 printf를 이용해서 위와같이 표현할수도 있다. 
+	
 	/*
 	1.234568
 	02
+	문자 하나 : a
+	문자열 출력 : 하이 나는 감팡이야 (str.c_str()의 반환은 첫 번째 char의 * pointer를 return한다 const *char)
 	*/
+	
+	return 0;
 }
-//형식 지정자 또한 알아야 출력 가능하다.
