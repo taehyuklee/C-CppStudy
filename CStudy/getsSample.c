e0bf61cce2591ef7c2d8f83b2a1ff2dfdee6c2f5
TayLee <89365465+taehyuklee@users.noreply.github.com>
2024-03-23 21:46:24 +0900

    Update getsSample.c

--- CStudy/getsSample.c
+++ CStudy/getsSample.c
@@ -3,6 +3,9 @@
 int main(void) {
 
 	char szName[32] = { 0 };
+	char szBuffer[8] = { 0 };
+	char lszBuffer[8] = { 0 };
+
 
 	/* 초기화된 메모리를 확인해보면 아래와 같이 0으로 초기화 된 것을 확인할 수 있다.
 		0x00000015898FF668  00 00 00 00 00 00 00 00  ........
@@ -24,6 +27,16 @@ int main(void) {
 		0x00000015898FF680  00 00 00 00 00 00 00 00  ........
 	*/
 	puts("입니다."); //printf는 개행이 되지 않지만, puts는 개행이 된다.
+
+	// 위의 gets가 문제가 보안상 문제가 있기 때문에, 
+	// gets_s를 써보자 gets의 경운 overflow가 일어나서 경계를 넘어간 쓰기를 하게 된다.
+	gets_s(szBuffer, sizeof(szBuffer));
+	puts(szBuffer);
+
+	//linux나 unix에서는 file gets를 사용한다 마지막에 file의 이름인 stdin까지 적어줘야 한다.
+	fgets(lszBuffer, sizeof(lszBuffer), stdin);
+	puts(lszBuffer);
+
 
 	return 0;
 }
